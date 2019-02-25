6f8400ee5dc39b0085a7f0a20241a13b2214006a
leecx <527437449@qq.com>
2019-02-25 17:34:40 +0800

    二分法

---
+++ leetCode/level1/合并两个有序链表/Solution.java
@@ -0,0 +1,43 @@
+package level1.合并两个有序链表;
+
+
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class Solution {
+
+    //类似并归排序
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        ListNode cur = listNode;
+        while(l1.next!=null && l2.next!=null){
+            if(l1.val > l2.val){
+
+
+            }else{
+
+            }
+        }
+
+
+        return listNode;
+    }
+
+
+}
+
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+
